9dbb34c02d8e4516352ef36c6899be183fae5168
Florian Thienel <florian@thienel.org>
2019-07-21 09:06:12 +0200

    cleanup

--- cmd/quad.go
+++ cmd/quad.go
@@ -22,7 +22,7 @@ var quadCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(quadCmd)
 
-	quadCmd.Flags().IntVar(&quadFlags.drive, "drive", 0, "the output drive strength in mA (2, 4, 6, 8)")
+	quadCmd.Flags().IntVar(&quadFlags.drive, "drive", 2, "the output drive strength in mA (2, 4, 6, 8)")
 	quadCmd.Flags().BoolVar(&quadFlags.noInit, "noInit", false, "do not initialize the Si5351")
 }
 
@@ -36,14 +36,10 @@ func runQuad(cmd *cobra.Command, args []string, device *si5351.Si5351) {
 	iOutput, err := parseOutput(args[1])
 	qOutput, err := parseOutput(args[2])
 	frequency, err := parseFrequency(args[3])
+	drive := toOutputDrive(quadFlags.drive)
 
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	drive := si5351.OutputDrive2mA
-	if quadFlags.drive != 0 {
-		drive = toOutputDrive(quadFlags.drive)
 	}
 
 	if !quadFlags.noInit {
